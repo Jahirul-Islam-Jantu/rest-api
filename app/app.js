b530b41a6782396613fb53ac5202f02dc7f9654a
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-17 14:54:14 +0600

    error handle on global routes and server

--- app/app.js
+++ app/app.js
@@ -1,13 +1,32 @@
+require('dotenv').config("../.env");
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
+app.use([
+    morgan('dev'),
+    cors(),
+    express.json(),
+])
 
 app.get('/health', (_req, res) => {
-    res.status(200).send('OK');
+    throw new Error("Error on server running");
+    res.status(200).json({"message": "Welcome to the server"});
+})
+
+app.use((_req, _res, next)=>{
+    const error = new Error("Resource not found")
+    error.status = 404
+    next(error)
+})
+app.use((error, _req, res, next)=>{
+    if (error.status){
+       return res.status(error.status).json({"message": error.message})
+    }
+    res.status(500).json({"message": error.message})
 })
 
 
-app.get('/', (req, res) => {
-    res.json("Hello World!");
-})
 
 module.exports = app;
