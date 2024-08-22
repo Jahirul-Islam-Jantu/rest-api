9e7f3e20946b9bf20fad820d5dff874c2c7a394e
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-22 22:32:45 +0600

    project ready

--- db/database.js
+++ db/database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/tickets').then(()=>{
+mongoose.connect('mongodb://127.0.0.1:27017/mongo').then(()=>{
     console.log("database connected");
 }).catch(err=>{
     console.log(err);
 }).finally(()=>{
     mongoose.connection.close()
 })
+
