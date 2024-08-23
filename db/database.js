124e1d9f5a452742d93c8219fa4afac3b1c75edb
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-23 22:12:51 +0600

    database timeout default if server not connected

--- db/database.js
+++ db/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/database').then(()=>{
+mongoose.connect('mongodb://127.0.0.1:27017/database', {serverSelectionTimeoutMS: 1000}).then(()=>{
     console.log("database connected");
 }).catch(err=>{
     console.log(err);
