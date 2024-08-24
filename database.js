845af3e7685cf04da43a8b19b29a41fa9c220601
Jahirul Islam <jahirulislamjantu002@gmail.com>
2024-08-24 08:37:36 +0600

    data saved on server

--- database.js
+++ database.js
@@ -18,10 +18,32 @@ const Person = mongoose.model("Person", personSchema);
 
 mongoose.connect('mongodb://localhost:27017/database', {serverSelectionTimeoutMS: 1000,}).then(async ()=>{
     console.log("database connected");
-    const person = new Person({})
+    const person = new Person({
+        firstName: "Jahirul",
+        lastName: "Islam",
+        age: 31,
+        bio: "This is Jahirul Islam jantu, son of Fazlur Rahman. He has 2 children . ",
+        single:false,
+        email: "jahirul@gmail.com",
+        phone: "01773427087",
+        address: "USA",
+        password: "jan742682",
+    })
+    const person2 = new Person({
+        firstName: "Samayrah",
+        lastName: "Islam",
+        age: 1,
+        bio: "This is Jahirul Islam jantu daughter.",
+        single:true,
+        email: "samayrah@gmail.com",
+        phone: "01995514304",
+        address: "USA",
+        password: "jan742682",
+    })
     await person.save();
+    await person2.save();
     console.log("Person saved successfully.")
-    console.log(person)
+    console.log(person, person2)
 }).catch(err=>{
     console.log(err);
 }).finally(()=>{
